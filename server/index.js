89e3ad8939c6f0a632ea2fc53e8a4b50fdce3d88
mmanohar24 <manojcareers24@gmail.com>
2026-05-11 16:20:24 -0700

    Set up Express server with initial route

---
+++ server/index.js
@@ -0,0 +1,29 @@
+// Importing packages
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+// Load env file
+dotenv.config();
+
+// Creating app and setting the port
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Define Routes
+app.get('/', (req, res) => {
+    res.json(
+        {
+            message: "Motor Archive API is running!!"
+        }
+    );
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
