4b7b00e50ef9172d8045e014e990a1ac51c93565
mmanohar24 <manojcareers24@gmail.com>
2026-05-11 22:14:22 -0700

    Connect MongoDB Atlas to Express server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const mongoose = require("mongoose")
 
 // Load env file
 dotenv.config();
@@ -13,6 +14,11 @@ const PORT = process.env.PORT || 5001;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((error) => console.log('MongoDB Connection Error:', error))
 
 // Define Routes
 app.get('/', (req, res) => {
