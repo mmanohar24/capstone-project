c020b761a49e2a13768f6ca891e79380acace92a
mmanohar24 <manojcareers24@gmail.com>
2026-05-14 19:25:53 -0700

    Move MongoDB connection to config/db.js

--- server/index.js
+++ server/index.js
@@ -2,23 +2,19 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const mongoose = require("mongoose")
+const connectDB = require('./config/db');
 
 // Load env file
 dotenv.config();
 
 // Creating app and setting the port
+connectDB();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((error) => console.log('MongoDB Connection Error:', error))
 
 // Define Routes
 app.get('/', (req, res) => {
